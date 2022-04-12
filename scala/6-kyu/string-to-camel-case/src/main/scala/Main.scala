197f0d69a4955d3ad88c41c047acc83d76773b60
André Ferreira <andresouzafe@gmail.com>
2022-04-12 19:19:56 -0300

    add scala exercises

--- scala/6-kyu/string-to-camel-case/src/main/scala/Main.scala
+++ scala/6-kyu/string-to-camel-case/src/main/scala/Main.scala
@@ -4,9 +4,8 @@
   println(output)
 
 def stringToCamel(arg: String): String =
-  if (arg.contains("-"))
+  if arg.contains("-") then
     arg.split("-")(0) ++ arg.split("-").slice(1, arg.split("-").length).map(str => str.capitalize).mkString
-  else if (arg.contains("_"))
+  else if arg.contains("_") then
     arg.split("_")(0) ++ arg.split("_").slice(1, arg.split("_").length).map(str => str.capitalize).mkString
   else ""
-
